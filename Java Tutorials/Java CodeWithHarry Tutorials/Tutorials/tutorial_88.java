690afafe4f54151b5c5b4e9f040f2d18c4b34c41
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-10-28 17:40:53 +0530

    Add files via upload

---
+++ Java Tutorials/Java CodeWithHarry Tutorials/Tutorials/tutorial_88.java
@@ -0,0 +1,18 @@
+/*
+Date 28-10-2021
+
+In this advance Java tutorial, you will learn about Collections Framework
+
+A collections framework is a unified architecture for representing and manipulating collections. All collections frameworks contain the following:
+Interfaces
+Implementations
+Algorithms
+
+Apart from the Java Collections Framework, the best-known examples of collections frameworks are the C++ Standard Template Library (STL) and Smalltalk's collection hierarchy. Historically, collections frameworks have been quite complex, which gave them a reputation for having a steep learning curve. We believe that the Java Collections Framework breaks with this tradition, as you will learn for yourself in this chapter.
+*/
+
+public class tutorial_88 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java Tutorials/Java CodeWithHarry Tutorials/Tutorials/tutorial_89.java
@@ -0,0 +1,17 @@
+/*
+Date 28-10-2021
+
+In this advance Java tutorial, you will learn about Collection Hierarchy
+Go here to know more about Java's  Collection Hierarchy ---> https://en.wikipedia.org/wiki/Java_collections_framework#/media/File:Java.util.Collection_hierarchy.svg
+*/
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class tutorial_89 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
